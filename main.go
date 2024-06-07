3f1679c0c135fb2dd93faf97abff57a6832c8ae5
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-06-07 20:46:09 +0300

    Trying stuff

--- main.go
+++ main.go
@@ -1,6 +1,27 @@
 package fmiopendata
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/Zabrakk/FmiOpenData/internal/models"
+	"github.com/Zabrakk/FmiOpenData/internal/http"
+)
+
+
+func Get() {
+	http.Get()
+}
+
+/*
+func GetModel() {
+	query := models.ObservationQuery{models.StoredQuery{"fmi::observation"}, "Helsinki", 12345}
+	fmt.Println(query.Id, query.Place, query.Fmisid)
+}
+*/
+
+func GetQuery() {
+	fmt.Println(models.GetDailyObservationStructForPlace("Helsinki"))
+	fmt.Println(models.GetDailyObservationStructForFmisid(12345))
+}
 
 func PrintText(text string) {
 	fmt.Print(text)
