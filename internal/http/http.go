ec372f157092af69308c38c3a8385cc52b6b7e6b
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-06-07 21:12:09 +0300

    Added SendQuery, renamed Get

--- internal/http/http.go
+++ internal/http/http.go
@@ -4,18 +4,37 @@ import (
 	"fmt"
 	"io"
 	nhttp "net/http"
+	"github.com/Zabrakk/FmiOpenData/internal/models"
+
 )
 
-func Get() {
-	resp, err := nhttp.Get("http://opendata.fmi.fi/wfs/eng?request=getFeature&storedquery_id=fmi::ef::stations&networkid=137&")
+func get(url string) string {
+	resp, err := nhttp.Get(url)
 	if (err != nil) {
 		fmt.Println("An error occured")
 		fmt.Println(err)
+		return ""
 	} else {
 		fmt.Println("Get was successful")
 		defer resp.Body.Close()
 		body, _ := io.ReadAll(resp.Body)
-		fmt.Println(string(body))
+		return string(body)
 	}
+}
 
+// TODO Interface
+
+func SendQuery(query models.ObservationQuery) string {
+	url := "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=GetFeature&storedquery_id="
+	url += query.Id
+	if len(query.Place) == 0 {
+		// TODO: Change string?
+		url += "&place=" + string(query.Fmisid)
+	} else {
+		url += "&place=" + query.Place
+	}
+	fmt.Println(url)
+	result := get(url)
+	fmt.Println(result)
+	return result
 }
