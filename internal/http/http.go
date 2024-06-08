6ae10787fbd49a3fe463cd31d6bca9b8e340fde8
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-06-08 15:39:39 +0300

    Added queryToUrlString

--- internal/http/http.go
+++ internal/http/http.go
@@ -3,11 +3,40 @@ package http
 import (
 	"io"
 	"fmt"
+	"strings"
+	"errors"
 	nhttp "net/http"
 
 	"github.com/Zabrakk/FmiOpenData/internal/models"
-
 )
+
+func queryToUrlString(query models.ObservationQuery) (string, error) {
+	if len(query.Place) < 1 && query.Fmisid == 0 {
+		return "", errors.New("ERROR: You must specify either Place or Fmisid for the ObservationQuery")
+	}
+	url := "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=GetFeature&storedquery_id="
+	url += query.Id
+	if query.Fmisid > 0 {
+		url += "&fmisid=" + fmt.Sprint(query.Fmisid)
+	} else {
+		url += "&place=" + query.Place
+	}
+	fmt.Println(query.StartTime)
+	if len(query.StartTime) > 0 {
+		url += "&starttime=" + query.StartTime
+	}
+	if len(query.EndTime) > 0 {
+		url += "&endtime=" + query.EndTime
+	}
+	if query.Timestep > 0 {
+		url += "&timestep=" + fmt.Sprint(query.Timestep)
+	}
+	if len(query.Parameters) > 0 {
+		url += "&parameters=" + strings.Join(query.Parameters[:], ",")
+	}
+	fmt.Println(url)
+	return url, nil
+}
 
 func get(url string) (io.ReadCloser, error) {
 	resp, err := nhttp.Get(url)
@@ -18,15 +47,10 @@ func get(url string) (io.ReadCloser, error) {
 	return resp.Body, nil
 }
 
-// TODO Interfaces
-
 func GetQueryResult(query models.ObservationQuery) (io.ReadCloser, error) {
-	url := "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=GetFeature&storedquery_id="
-	url += query.Id
-	if len(query.Place) == 0 {
-		url += "&fmisid=" + fmt.Sprint(query.Fmisid)
-	} else {
-		url += "&place=" + query.Place
+	url, err := queryToUrlString(query)
+	if err != nil {
+		return nil, err
 	}
 	fmt.Println(url)
 	result, err := get(url)
