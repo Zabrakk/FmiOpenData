68f4c498bfc18bac99513cc16132c44dfab18518
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-06-08 15:39:18 +0300

    Query creation updates

--- main.go
+++ main.go
@@ -9,15 +9,29 @@ import (
 	"github.com/Zabrakk/FmiOpenData/internal/xmlparser"
 )
 
+func GetDailyObservationQuery() models.ObservationQuery {
+	query := models.ObservationQuery{}
+	query.Id = "fmi::observations::weather::daily::timevaluepair"
+	return query
+}
 
-func GetQueryResult() {
-	query := models.GetDailyObservationStructForPlace("Helsinki")
+func GetHourlyObservationQuery() models.ObservationQuery {
+	query := models.ObservationQuery{}
+	query.Id = "fmi::observations::weather::hourly::timevaluepair"
+	return query
+}
+
+func TimeToQueryFormat(t time.Time) string {
+	return t.Format("2006-01-02T00:00:00Z")
+}
+
+func GetQueryResult(query models.ObservationQuery) {
 	queryResult, err := http.GetQueryResult(query)
-	defer queryResult.Close() // Close when this function's execution ends.
 	if err != nil {
-		fmt.Println("ERROR!!!")
+		fmt.Println(err)
 		return
 	}
+	defer queryResult.Close() // Close when this function's execution ends.
 	result, err := xmlparser.ParseQueryResult(queryResult)
 	if err != nil {
 		fmt.Println("ERROR WHILE PARSING XML!!!")
@@ -26,14 +40,4 @@ func GetQueryResult() {
 	for _, m := range result.Precipitation() {
 		fmt.Println(m.Value, m.Time)
 	}
-}
-
-func GetDailyObservationQuery() models.StoredQuery {
-	query := models.StoredQuery{}
-	query.Id = "fmi::observations::weather::daily::timevaluepair"
-	return query
-}
-
-func TimeToQueryFormat(t time.Time) string {
-	return t.Format("2006-01-02T00:00:00Z")
 }
