2bb905d16f43d824d78d919fb74a947d8b06c76b
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-06-08 13:39:47 +0300

    Updates: -Added DailyMeasurements struct that contains an array of timeseries -Added getters for measurements

--- internal/models/measurements.go
+++ internal/models/measurements.go
@@ -1,20 +1,68 @@
 package models
 
-import "encoding/xml"
+import (
+	"strings"
+	"encoding/xml"
+)
 
-type MeasurementTimeseries struct {
-	MeasurementName string
-	XMLName xml.Name `xml:"MeasurementTimeseries"`
-	Points []Point `xml:"point"`
+type DailyMeasrurements struct {
+	MeasurementTimeseries []MeasurementTimeseries
 }
 
+// Measurement timeseries includes measurement name, e.g. obs-obs-1-1-tday,
+// and an array of measurement points.
+type MeasurementTimeseries struct {
+	Name string
+	XMLName xml.Name `xml:"MeasurementTimeseries"`
+	Measurements []Point `xml:"point"`
+}
+
+// A measurement point includes one measurement time-value pair
 type Point struct {
 	XMLName xml.Name `xml:"point"`
-	Observation Obs `xml:"MeasurementTVP"`
+	Measurement MeasurementTVP `xml:"MeasurementTVP"`
 }
 
-type Obs struct {
+// Measurement Time-Value Pair. Includes the measurement's time and value
+type MeasurementTVP struct {
 	XMLName xml.Name `xml:"MeasurementTVP"`
 	Time string	`xml:"time"`
 	Value string `xml:"value"`
+}
+
+func get_mts(dm DailyMeasrurements, name string) []MeasurementTVP {
+	var result []MeasurementTVP
+	for _, mts := range dm.MeasurementTimeseries {
+		if strings.Contains(mts.Name, name) {
+			for _, point := range mts.Measurements {
+				result = append(result, point.Measurement)
+			}
+			return result
+		}
+	}
+	return nil
+}
+
+func (dm DailyMeasrurements) Precipitation() []MeasurementTVP {
+	return get_mts(dm, "rrday")
+}
+
+func (dm DailyMeasrurements) AirTemperature() []MeasurementTVP {
+	return get_mts(dm, "tday")
+}
+
+func (dm DailyMeasrurements) MinimumTemperature() []MeasurementTVP {
+	return get_mts(dm, "tmin")
+}
+
+func (dm DailyMeasrurements) MaximumTemperature() []MeasurementTVP {
+	return get_mts(dm, "tmax")
+}
+
+func (dm DailyMeasrurements) GroundMinimumTemperature() []MeasurementTVP {
+	return get_mts(dm, "TG_PT12H_min")
+}
+
+func (dm DailyMeasrurements) SnowDepth() []MeasurementTVP {
+	return get_mts(dm, "snow")
 }
