a7328d4ec5d5da2a313b9092b750871663c1e3a4
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-06-09 18:13:49 +0300

    Added setters for ObservationQuery

--- models/queries.go
+++ models/queries.go
@@ -1,6 +1,10 @@
 package models
 
-type StoredQuery struct {
+import(
+	"time"
+)
+
+type storedQuery struct {
 	Id			string
 	StartTime	string
 	EndTime		string
@@ -8,8 +12,36 @@ type StoredQuery struct {
 }
 
 type ObservationQuery struct {
-	StoredQuery // Anonymous field
+	storedQuery // Anonymous field
 	Place 		string
 	Fmisid 		int
 	Timestep	int
+}
+
+func timeToQueryFormat(t time.Time) string {
+	return t.Format("2006-01-02T00:00:00Z")
+}
+
+func (q *ObservationQuery) SetPlace(place string) {
+	q.Place = place
+}
+
+func (q *ObservationQuery) SetFmisid(fmisid int) {
+	q.Fmisid = fmisid
+}
+
+func (q *ObservationQuery) SetStartTime(startTime time.Time) {
+	q.StartTime = timeToQueryFormat(startTime)
+}
+
+func (q *ObservationQuery) SetEndTime(endTime time.Time) {
+	q.EndTime = timeToQueryFormat(endTime)
+}
+
+func (q *ObservationQuery) SetTimestep(timestep int) {
+	q.Timestep = timestep
+}
+
+func (q *ObservationQuery) SetParameters(parameters []string) {
+	q.Parameters = parameters
 }
