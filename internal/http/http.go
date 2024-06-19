f8abfadd08275a0c1d35a8d656d2a590619f93eb
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-06-19 19:33:39 +0300

    More parameters in queryToUrlString

--- internal/http/http.go
+++ internal/http/http.go
@@ -11,15 +11,17 @@ import (
 )
 
 func queryToUrlString(query models.ObservationQuery) (string, error) {
-	if len(query.Place) < 1 && query.Fmisid == 0 {
-		return "", errors.New("ERROR: You must specify either Place or Fmisid for the ObservationQuery")
+	if len(query.Place) < 1 && query.Fmisid == 0 && len(query.Bbox) < 1 {
+		return "", errors.New("ERROR: You must specify either Place, Fmisid or Bbox for the ObservationQuery")
 	}
 	url := "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=GetFeature&storedquery_id="
 	url += query.Id
 	if query.Fmisid > 0 {
 		url += "&fmisid=" + fmt.Sprint(query.Fmisid)
-	} else {
+	} else if len(query.Place) > 0 {
 		url += "&place=" + query.Place
+	} else {
+		url += "&bbox=" + query.Bbox
 	}
 	fmt.Println(query.StartTime)
 	if len(query.StartTime) > 0 {
@@ -33,6 +35,9 @@ func queryToUrlString(query models.ObservationQuery) (string, error) {
 	}
 	if len(query.Parameters) > 0 {
 		url += "&parameters=" + strings.Join(query.Parameters[:], ",")
+	}
+	if query.MaxLocations > 0 {
+		url += "&maxlocations=" + fmt.Sprint(query.MaxLocations)
 	}
 	return url, nil
 }
