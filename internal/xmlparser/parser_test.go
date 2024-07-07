8bce8d96367f124cd2d29440399e7b2bd9aa23cc
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-07-07 20:09:15 +0300

    Added utest for ParseExplainParamResult

---
+++ internal/xmlparser/parser_test.go
@@ -0,0 +1,22 @@
+package xmlparser
+
+import (
+	"testing"
+)
+
+func TestParseExplainParameterResult(t *testing.T) {
+	xmlFile := "<ObservableProperty><label>Test1</label><basePhenomenon>Test2</basePhenomenon><uom uom=\"Test3\"/></ObservableProperty>"
+	result, err := ParseExplainParamResult([]byte(xmlFile))
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	if result.Label != "Test1" {
+		t.Fatalf("ExplainedParam Label was incorrect: %s", result.Label)
+	}
+	if result.BasePhenomenon != "Test2" {
+		t.Fatalf("ExplainedParam BasePhenomenon was incorrect: %s", result.BasePhenomenon)
+	}
+	if result.UOM.Value != "Test3" {
+		t.Fatalf("ExplainedParam UOM value was incorrect: %s", result.UOM.Value)
+	}
+}
