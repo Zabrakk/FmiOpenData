0d808b0dd52b21d915eeed0bef77f6279519f833
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-07-09 20:19:08 +0300

    Refactoring + more testing ParseExplainParamResult

--- internal/xmlparser/parser_test.go
+++ internal/xmlparser/parser_test.go
@@ -5,7 +5,11 @@ import (
 )
 
 func TestParseExplainParameterResult(t *testing.T) {
-	xmlFile := "<ObservableProperty><label>Test1</label><basePhenomenon>Test2</basePhenomenon><uom uom=\"Test3\"/></ObservableProperty>"
+	xmlFile := `<ObservableProperty>
+		<label>Test1</label>
+		<basePhenomenon>Test2</basePhenomenon>
+		<uom uom="Test3"/>
+	</ObservableProperty>`
 	result, err := ParseExplainParamResult([]byte(xmlFile))
 	if err != nil {
 		t.Fatalf("%s", err)
@@ -18,5 +22,19 @@ func TestParseExplainParameterResult(t *testing.T) {
 	}
 	if result.UOM.Value != "Test3" {
 		t.Fatalf("ExplainedParam UOM value was incorrect: %s", result.UOM.Value)
+	}
+}
+
+func TestParseExplainParamResultMissingField(t *testing.T) {
+	xmlFile := `<ObservableProperty>
+		<label>Test1</label>
+		<basePhenomenon>Test2</basePhenomenon>
+	</ObservableProperty>`
+	result, err := ParseExplainParamResult([]byte(xmlFile))
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	if result.UOM.Value != "" {
+		t.Fatalf("Did not handle missing field correctly")
 	}
 }
