2bd1c56e0c780369bda7482442e46a616aa944f1
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-07-09 20:46:53 +0300

    Added TestParseQueryResult

--- internal/xmlparser/parser_test.go
+++ internal/xmlparser/parser_test.go
@@ -1,6 +1,8 @@
 package xmlparser
 
 import (
+	"io"
+	"strings"
 	"testing"
 )
 
@@ -36,5 +38,62 @@ func TestParseExplainParamResultMissingField(t *testing.T) {
 	}
 	if result.UOM.Value != "" {
 		t.Fatalf("Did not handle missing field correctly")
+	}
+}
+
+func TestParseQueryResult(t *testing.T) {
+	xmlFile := strings.NewReader(`
+		<wml2:MeasurementTimeseries gml:id="obs-obs-1-1-rrday">
+			<wml2:point>
+				<wml2:MeasurementTVP>
+					<wml2:time>2024-06-09T00:00:00Z</wml2:time>
+					<wml2:value>16.0</wml2:value>
+				</wml2:MeasurementTVP>
+			</wml2:point>
+			<wml2:point>
+				<wml2:MeasurementTVP>
+					<wml2:time>2024-06-09T06:00:00Z</wml2:time>
+					<wml2:value>17.0</wml2:value>
+				</wml2:MeasurementTVP>
+			</wml2:point>
+		</wml2:MeasurementTimeseries>
+		<wml2:MeasurementTimeseries gml:id="obs-obs-1-1-tday">
+			<wml2:point>
+				<wml2:MeasurementTVP>
+					<wml2:time>2024-06-09T00:00:00Z</wml2:time>
+					<wml2:value>16.0</wml2:value>
+				</wml2:MeasurementTVP>
+			</wml2:point>
+			<wml2:point>
+				<wml2:MeasurementTVP>
+					<wml2:time>2024-06-09T06:00:00Z</wml2:time>
+					<wml2:value>17.0</wml2:value>
+				</wml2:MeasurementTVP>
+			</wml2:point>
+		</wml2:MeasurementTimeseries>
+	`)
+	xmlFile2 := io.NopCloser(xmlFile)
+	result, err := ParseQueryResult(xmlFile2)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	if len(result.MeasurementTimeseries) != 2 {
+		t.Fatalf("Incorrect number of MeasurementTimeseries: %d", len(result.MeasurementTimeseries))
+	}
+	measurementNames := []string{"rrday", "tday"}
+	measurementTimes := []string{"2024-06-09T00:00:00Z", "2024-06-09T06:00:00Z", }
+	measurementValues := []string{"16.0", "17.0"}
+	for i := 0; i < 2; i++ {
+		if result.MeasurementTimeseries[i].Name != measurementNames[i] {
+			t.Fatalf("Inccorect measurement name for first entry: %s", result.MeasurementTimeseries[i].Name)
+		}
+		for j:= 0; j < 2; j++ {
+			if result.MeasurementTimeseries[i].Measurements[j].Time != measurementTimes[j] {
+				t.Fatalf("Incorrect measurement time for entry i:%d j:%d", i, j)
+			}
+			if result.MeasurementTimeseries[i].Measurements[j].Value != measurementValues[j] {
+				t.Fatalf("Incorrect measurement value for entry i:%d j:%d", i, j)
+			}
+		}
 	}
 }
