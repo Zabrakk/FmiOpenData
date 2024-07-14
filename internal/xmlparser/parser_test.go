7df545b23a3c7fda31ac675922e8d157a2a5db2f
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-07-14 19:10:49 +0300

    Refactoring

--- internal/xmlparser/parser_test.go
+++ internal/xmlparser/parser_test.go
@@ -4,6 +4,8 @@ import (
 	"io"
 	"strings"
 	"testing"
+
+	"github.com/Zabrakk/FmiOpenData/models"
 )
 
 func TestParseExplainParameterResult(t *testing.T) {
@@ -41,104 +43,87 @@ func TestParseExplainParamMissingField(t *testing.T) {
 	}
 }
 
+var measurementNames = []string{"rrday", "tday"}
+var measurementTimes = []string{"2024-06-09T00:00:00Z", "2024-06-09T06:00:00Z" }
+var measurementValues = []string{"16.0", "17.0"}
+
 func TestParseMeasurementTimeseries(t *testing.T) {
-	xmlFile := strings.NewReader(`
-		<wml2:MeasurementTimeseries gml:id="obs-obs-1-1-rrday">
+	xmlFile := io.NopCloser(strings.NewReader(`
+		<wml2:MeasurementTimeseries gml:id="obs-obs-1-1-` + measurementNames[0] + `">
 			<wml2:point>
 				<wml2:MeasurementTVP>
-					<wml2:time>2024-06-09T00:00:00Z</wml2:time>
-					<wml2:value>16.0</wml2:value>
+					<wml2:time>` + measurementTimes[0] + `</wml2:time>
+					<wml2:value>` + measurementValues[0] + `</wml2:value>
 				</wml2:MeasurementTVP>
 			</wml2:point>
 			<wml2:point>
 				<wml2:MeasurementTVP>
-					<wml2:time>2024-06-09T06:00:00Z</wml2:time>
-					<wml2:value>17.0</wml2:value>
-				</wml2:MeasurementTVP>
-			</wml2:point>
-		</wml2:MeasurementTimeseries>
-		<wml2:MeasurementTimeseries gml:id="obs-obs-1-1-tday">
-			<wml2:point>
-				<wml2:MeasurementTVP>
-					<wml2:time>2024-06-09T00:00:00Z</wml2:time>
-					<wml2:value>16.0</wml2:value>
-				</wml2:MeasurementTVP>
-			</wml2:point>
-			<wml2:point>
-				<wml2:MeasurementTVP>
-					<wml2:time>2024-06-09T06:00:00Z</wml2:time>
-					<wml2:value>17.0</wml2:value>
+					<wml2:time>` + measurementTimes[1] + `</wml2:time>
+					<wml2:value>` + measurementValues[1] + `</wml2:value>
 				</wml2:MeasurementTVP>
 			</wml2:point>
 		</wml2:MeasurementTimeseries>
-	`)
-	xmlFile2 := io.NopCloser(xmlFile)
-	result, err := ParseMeasurementTimeseries(xmlFile2)
-	if err != nil {
-		t.Fatalf("%s", err)
-	}
-	if len(result.MeasurementTimeseries) != 2 {
-		t.Fatalf("Incorrect number of MeasurementTimeseries: %d", len(result.MeasurementTimeseries))
-	}
-	measurementNames := []string{"rrday", "tday"}
-	measurementTimes := []string{"2024-06-09T00:00:00Z", "2024-06-09T06:00:00Z", }
-	measurementValues := []string{"16.0", "17.0"}
-	for i := 0; i < 2; i++ {
-		if result.MeasurementTimeseries[i].Name != measurementNames[i] {
-			t.Fatalf("Inccorect measurement name for first entry: %s", result.MeasurementTimeseries[i].Name)
-		}
-		for j:= 0; j < 2; j++ {
-			if result.MeasurementTimeseries[i].Measurements[j].Time != measurementTimes[j] {
-				t.Fatalf("Incorrect measurement time for entry i:%d j:%d", i, j)
-			}
-			if result.MeasurementTimeseries[i].Measurements[j].Value != measurementValues[j] {
-				t.Fatalf("Incorrect measurement value for entry i:%d j:%d", i, j)
-			}
-		}
-	}
+		<wml2:MeasurementTimeseries gml:id="obs-obs-1-1-` + measurementNames[1] + `">
+			<wml2:point>
+				<wml2:MeasurementTVP>
+					<wml2:time>` + measurementTimes[0] + `</wml2:time>
+					<wml2:value>` + measurementValues[0] + `</wml2:value>
+				</wml2:MeasurementTVP>
+			</wml2:point>
+			<wml2:point>
+				<wml2:MeasurementTVP>
+					<wml2:time>` + measurementTimes[1] + `</wml2:time>
+					<wml2:value>` + measurementValues[1] + `</wml2:value>
+				</wml2:MeasurementTVP>
+			</wml2:point>
+		</wml2:MeasurementTimeseries>
+	`))
+	result, err := ParseMeasurementTimeseries(xmlFile)
+	checkAllMeasurementsCorrectness(result, err, t)
 }
 
 func TestParseBsWfsElements(t *testing.T) {
 	xmlFile := io.NopCloser(strings.NewReader(`
 		<wfs:member>
 			<BsWfs:BsWfsElement>
-				<BsWfs:Time>2024-06-09T00:00:00Z</BsWfs:Time>
-				<BsWfs:ParameterName>t2m</BsWfs:ParameterName>
-				<BsWfs:ParameterValue>16.0</BsWfs:ParameterValue>
+				<BsWfs:Time>` + measurementTimes[0] + `</BsWfs:Time>
+				<BsWfs:ParameterName>` + measurementNames[0] + `</BsWfs:ParameterName>
+				<BsWfs:ParameterValue>` + measurementValues[0] + `</BsWfs:ParameterValue>
 			</BsWfs:BsWfsElement>
 		</wfs:member>
 		<wfs:member>
 			<BsWfs:BsWfsElement>
-				<BsWfs:Time>2024-06-09T00:00:00Z</BsWfs:Time>
-				<BsWfs:ParameterName>ws_10min</BsWfs:ParameterName>
-				<BsWfs:ParameterValue>16.0</BsWfs:ParameterValue>
+				<BsWfs:Time>` + measurementTimes[0] + `</BsWfs:Time>
+				<BsWfs:ParameterName>` + measurementNames[1] + `</BsWfs:ParameterName>
+				<BsWfs:ParameterValue>` + measurementValues[0] + `</BsWfs:ParameterValue>
 			</BsWfs:BsWfsElement>
 		</wfs:member>
 		<wfs:member>
 			<BsWfs:BsWfsElement>
-				<BsWfs:Time>2024-06-09T06:00:00Z</BsWfs:Time>
-				<BsWfs:ParameterName>t2m</BsWfs:ParameterName>
-				<BsWfs:ParameterValue>17.0</BsWfs:ParameterValue>
+				<BsWfs:Time>` + measurementTimes[1] + `</BsWfs:Time>
+				<BsWfs:ParameterName>` + measurementNames[0] + `</BsWfs:ParameterName>
+				<BsWfs:ParameterValue>` + measurementValues[1] + `</BsWfs:ParameterValue>
 			</BsWfs:BsWfsElement>
 		</wfs:member>
 		<wfs:member>
 			<BsWfs:BsWfsElement>
-				<BsWfs:Time>2024-06-09T06:00:00Z</BsWfs:Time>
-				<BsWfs:ParameterName>ws_10min</BsWfs:ParameterName>
-				<BsWfs:ParameterValue>17.0</BsWfs:ParameterValue>
+				<BsWfs:Time>` + measurementTimes[1] + `</BsWfs:Time>
+				<BsWfs:ParameterName>` + measurementNames[1] + `</BsWfs:ParameterName>
+				<BsWfs:ParameterValue>` + measurementValues[1] + `</BsWfs:ParameterValue>
 			</BsWfs:BsWfsElement>
 		</wfs:member>
 	`))
 	result, err := ParseBsWfsElements(xmlFile)
+	checkAllMeasurementsCorrectness(result, err, t)
+}
+
+func checkAllMeasurementsCorrectness(result models.AllMeasurements, err error, t *testing.T) {
 	if err != nil {
 		t.Fatalf("%s", err)
 	}
 	if len(result.MeasurementTimeseries) != 2 {
 		t.Fatalf("Incorrect number of MeasurementTimeseries: %d", len(result.MeasurementTimeseries))
 	}
-	measurementNames := []string{"t2m", "ws_10min"}
-	measurementTimes := []string{"2024-06-09T00:00:00Z", "2024-06-09T06:00:00Z" }
-	measurementValues := []string{"16.0", "17.0"}
 	for i := 0; i < 2; i++ {
 		if result.MeasurementTimeseries[i].Name != measurementNames[i] {
 			t.Fatalf("Inccorect measurement name for first entry: %s", result.MeasurementTimeseries[i].Name)
